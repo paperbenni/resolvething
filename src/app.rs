b03eef8c6a84ddaa19272f6bafc0582bff8b0a0a
paperbenni <paperbenni@gmail.com>
2025-04-15 13:21:28 +0200

    add dependency checker

--- src/app.rs
+++ src/app.rs
@@ -6,8 +6,33 @@ pub struct App {
 
 impl App {
     pub fn new() -> Self {
+        Self::check_dependencies();
         Self {
             config: Config::load(),
+        }
+    }
+
+    /// checks if a command is installed
+    pub fn check_command(command: &str) -> bool {
+        if which::which(command).is_err() {
+            eprintln!("Command '{}' not found. Please install.", command);
+            return false;
+        }
+        true
+    }
+
+    pub fn check_dependencies() {
+        let commands = vec!["fdupes", "fzf", "blender"];
+        let mut any_missing = false;
+        for command in commands {
+            if !Self::check_command(command) {
+                any_missing = true;
+            }
+        }
+        if any_missing {
+            eprintln!("Some dependencies are missing. Please install them to use this program.");
+            //TODO: this is horrible, do it better
+            std::process::exit(1);
         }
     }
 
